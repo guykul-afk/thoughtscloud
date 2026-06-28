9e765bd493441d8b327e2aff407fe148ae4dd688
guy kul <guykul@gmail.com>
2026-06-28 11:57:06 +0300

    refactor: Relocate OKF Web Viewer to a standalone directory

--- viewer/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Standard Vite config for Firebase-only operations
-export default defineConfig({
-  plugins: [react()],
-})
